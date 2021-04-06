e1de18beebe4d794ef8fe615da8a01ed664d640a
Joe Witzke <joewitzke@joes-mbp.lan>
2021-04-06 14:42:52 -0600

    Initial commit with some onload and nav functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-// Your code goes here
+//On Load
+window.addEventListener('load', function(event){
+    alert('Welcome to Fun Bus!!');
+})
+
+// Navbar
+const navLinks = document.querySelectorAll('.nav .nav-link');
+const navBackground = document.querySelector('.main-navigation');
+const navHeader = document.querySelector('.logo-heading');
+
+navHeader.addEventListener('mouseover', function(event){
+    navHeader.style.color = 'fuchsia';
+});
+navBackground = document.querySelector('scroll', function(event){
+    navBackground.style.backgroundColor = "aqua";
+})
+
