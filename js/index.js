a5cf6d03e5356bbb7d51191d1c836adc458d5865
Joe Witzke <joewitzke@joes-mbp.lan>
2021-04-06 15:58:48 -0600

    added some events to the bus img

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 //On Load
 window.addEventListener('load', function(event){
     alert('Welcome to Fun Bus!!');
-})
+});
 
 // Navbar
 const navLinks = document.querySelectorAll('.nav .nav-link');
@@ -11,7 +11,25 @@ const navHeader = document.querySelector('.logo-heading');
 navHeader.addEventListener('mouseover', function(event){
     navHeader.style.color = 'fuchsia';
 });
-navBackground = document.querySelector('scroll', function(event){
-    navBackground.style.backgroundColor = "aqua";
-})
+// navBackground = document.querySelector('mouseover', function(event){
+//     navBackground.style.backgroundColor = "aqua";
+// })
+
+//h2 subtitles
+const welcomeSub = document.querySelector(".intro h2");
+const busImg = document.querySelector('.intro img');
+
+welcomeSub.addEventListener('mouseover', function(event){
+    if(welcomeSub.style.texTransform !== 'uppercase'){
+        welcomeSub.style.texTransform = 'uppercase'
+    }else{
+        welcomeSub.style.texTransform = 'lowercase'
+    }
+});
+
+busImg.addEventListener('dblclick', function(event){
+    busImg.src = ('https://images.unsplash.com/photo-1470472304068-4398a9daab00?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGFkdmVudHVyZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60')
+});
+
+
 
