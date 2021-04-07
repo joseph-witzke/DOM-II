75159b805ba91d01b444ac66939b08c226857228
Joe Witzke <joewitzke@joes-mbp.lan>
2021-04-06 20:53:05 -0600

    Added some 'click' events to the h4s

--- js/index.js
+++ js/index.js
@@ -24,12 +24,41 @@ busImg.addEventListener('dblclick', function(event){
     busImg.src = ('https://images.unsplash.com/photo-1470472304068-4398a9daab00?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGFkdmVudHVyZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60')
 });
 
+
 //background color
 const background = document.querySelector(".content-section");
+const background2 = document.querySelector(".content-section.inverse-content");
+const background3 = document.querySelector(".content-destination");
+const background4 = document.querySelector(".intro h2");
 
 
 background.addEventListener('mouseover', function(event){
     background.style.backgroundColor = "blue";
 })
+
+background2.addEventListener('mouseover', function(event){
+    background2.style.backgroundColor = "aqua";
+})
+
+background3.addEventListener('mouseover', function(event){
+    background3.style.backgroundColor = "darksalmon";
+});
+
+background4.addEventListener('mouseover', function(event){
+    background4.style.backgroundColor = "pink";
+});
+
+const bottomSubs = document.querySelectorAll("h4")
+
+bottomSubs[0].addEventListener('click', function(event){
+    bottomSubs[0].style.color = 'pink';
+});
+bottomSubs[1].addEventListener('click', function(event){
+    bottomSubs[1].style.color = 'cyan';
+});
+bottomSubs[2].addEventListener('click', function(event){
+    bottomSubs[2].style.color = 'darkmagenta';
+});
+
 
 
